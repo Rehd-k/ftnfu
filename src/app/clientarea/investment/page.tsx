be7c641231a745212c2d98a69479f1140c8f3cd4
Rhed <coolmirth@gmail.com>
2024-04-06 22:50:47 +0100

    omo ehn

--- src/app/clientarea/investment/page.tsx
+++ src/app/clientarea/investment/page.tsx
@@ -4,7 +4,9 @@ import InvestmentModel from "@/model/investments";
 import Investments from "./components/investmentCard";
 export default async function Investment() {
 
-    const investments = await InvestmentModel.find()
+    // const investments = await InvestmentModel.find()
+
+    const investments: any[] = []
     return  <div className="md:w-11/12 mx-auto mt-5">
         {investments.length <= 0 ? <Preloaders /> :
 
